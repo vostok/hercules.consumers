fe0775b86205fed2b440bb8a6181cd6ec6c484a5
epeshk <peshkov@skbkontur.ru>
2019-05-23 16:37:37 +0500

    Added InMemoryCoordinatesStorage for test purposes.

---
+++ Vostok.Hercules.Consumers/InMemoryCoordinatesStorage.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Models;
+using Vostok.Hercules.Consumers.Helpers;
+
+namespace Vostok.Hercules.Consumers
+{
+    [PublicAPI]
+    public class InMemoryCoordinatesStorage : IStreamCoordinatesStorage
+    {
+        private readonly SemaphoreSlim advanceSemaphore = new SemaphoreSlim(1, 1);
+        private volatile StreamCoordinates coordinates;
+
+        public InMemoryCoordinatesStorage(StreamCoordinates initialCoordinates) => coordinates = initialCoordinates;
+
+        public InMemoryCoordinatesStorage()
+            : this(new StreamCoordinates(Array.Empty<StreamPosition>()))
+        {
+        }
+
+        public Task<StreamCoordinates> GetCurrentAsync() => Task.FromResult(coordinates);
+
+        public async Task AdvanceAsync(StreamCoordinates to)
+        {
+            if (!await advanceSemaphore.WaitAsync(TimeSpan.Zero).ConfigureAwait(false))
+                return;
+
+            try
+            {
+                var currentCoordinates = coordinates;
+
+                if (!to.AdvancesOver(currentCoordinates))
+                    return;
+
+                var mergedCoordinates = StreamCoordinatesMerger.Merge(currentCoordinates, to);
+
+                coordinates = mergedCoordinates;
+            }
+            finally
+            {
+                advanceSemaphore.Release();
+            }
+        }
+    }
+}
