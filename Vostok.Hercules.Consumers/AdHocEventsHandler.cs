ff9c6e7aa31f92d5e9928408396d9a94602d16f6
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-23 16:33:23 +0500

    AdHocEventsHandler

---
+++ Vostok.Hercules.Consumers/AdHocEventsHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Hercules.Client.Abstractions.Models;
+
+namespace Vostok.Hercules.Consumers
+{
+    [PublicAPI]
+    public class AdHocEventsHandler : IStreamEventsHandler
+    {
+        private readonly Func<StreamCoordinates, IList<HerculesEvent>, CancellationToken, Task> handler;
+
+        public AdHocEventsHandler(Func<StreamCoordinates, IList<HerculesEvent>, CancellationToken, Task> handler)
+        {
+            this.handler = handler;
+        }
+
+        public Task HandleAsync(StreamCoordinates coordinates, IList<HerculesEvent> events, CancellationToken cancellationToken) =>
+            handler(coordinates, events, cancellationToken);
+    }
+}
