45f7838c77067dcb2c0c25783cad71e4ebbf03f8
kungurtsev <kungurtsev@skbkontur.ru>
2022-08-15 17:06:51 +0500

    use channels & buffers

--- Vostok.Hercules.Consumers/StreamBinaryEventsWriter.cs
+++ Vostok.Hercules.Consumers/StreamBinaryEventsWriter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#if NET6_0_OR_GREATER
+
+using System;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Vostok.Commons.Binary;
@@ -7,6 +10,11 @@ using Vostok.Hercules.Client;
 using Vostok.Hercules.Client.Abstractions.Events;
 using Vostok.Hercules.Client.Serialization.Builders;
 using Vostok.Logging.Abstractions;
+using Vostok.Metrics.Grouping;
+using Vostok.Metrics.Primitives.Timer;
+using Vostok.Tracing.Extensions.Custom;
+
+// ReSharper disable ParameterHidesMember
 
 namespace Vostok.Hercules.Consumers
 {
@@ -15,46 +23,109 @@ namespace Vostok.Hercules.Consumers
     {
         private readonly StreamBinaryEventsWriterSettings settings;
         private readonly ILog log;
-        private readonly BinaryBufferWriter buffer;
-        private int eventsCount;
 
-        public StreamBinaryEventsWriter([NotNull] StreamBinaryEventsWriterSettings settings, [CanBeNull] ILog log)
+        private readonly Channel<Writer> emptyWriters;
+        private readonly IMetricGroup1<ITimer> iterationMetric;
+
+        private Writer writer;
+
+        public StreamBinaryEventsWriter(StreamBinaryEventsWriterSettings settings, ILog log)
         {
             this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
-            this.log = log = (log ?? LogProvider.Get()).ForContext<StreamBinaryEventsWriter>();
+            this.log = (log ?? LogProvider.Get()).ForContext<StreamBinaryEventsWriter>();
 
-            buffer = new BinaryBufferWriter(0) {Endianness = Endianness.Big};
-            buffer.Write(0);
+            iterationMetric = settings.MetricContext.CreateSummary("iteration", "type", new SummaryConfig {Quantiles = new[] {0.5, 0.75, 1}});
+
+            emptyWriters = Channel.CreateUnbounded<Writer>(new UnboundedChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = false,
+                AllowSynchronousContinuations = false
+            });
+
+            for (var i = 0; i < settings.WritersPoolCapacity; i++)
+                emptyWriters.Writer.WriteAsync(new Writer(settings.WriterCapacity.Bytes)).AsTask().GetAwaiter().GetResult();
         }
 
         public void Put(Action<IHerculesEventBuilder> buildEvent)
         {
-            if (buffer.Length > settings.BufferCapacityLimit)
+            if (writer == null || writer.RemainingCapacity < settings.MaximumEventSize || DateTime.UtcNow - writer.WritingStarted > settings.WriterMaxTtl)
             {
-                log.Warn("Buffer capacity {Capacity} exceeded. Trigger write.", buffer.Length);
-                WriteAsync().GetAwaiter().GetResult();
+                if (writer != null)
+                {
+                    var fullWriter = writer;
+                    Task.Run(() => FlushWriter(fullWriter));
+                }
+
+                writer = ObtainEmptyWriter();
             }
 
-            eventsCount++;
+            writer.StartEvent();
 
-            using (var eventBuilder = new BinaryEventBuilder(buffer, () => PreciseDateTime.UtcNow, Constants.EventProtocolVersion))
-            {
-                buildEvent(eventBuilder);
-            }
+            using var eventBuilder = new BinaryEventBuilder(writer, () => PreciseDateTime.UtcNow, Constants.EventProtocolVersion);
+
+            buildEvent(eventBuilder);
         }
 
-        public async Task WriteAsync()
+        public Task FlushAsync() =>
+            FlushWriter(writer);
+
+        private Writer ObtainEmptyWriter()
         {
-            using (buffer.JumpTo(0))
+            using var _ = settings.Tracer.BeginCustomOperationSpan("WaitWriter");
+            using var __ = iterationMetric.For("waitWriter_time").Measure();
+
+            return emptyWriters.Reader.ReadAsync().AsTask().GetAwaiter().GetResult();
+        }
+
+        private async Task FlushWriter(Writer writer)
+        {
+            writer.EndWriting();
+
+            await settings.StreamBinaryWriter.WriteAsync(settings.StreamName, writer.FilledSegment, writer.EventsCount).ConfigureAwait(false);
+
+            writer.Reset();
+            
+            await emptyWriters.Writer.WriteAsync(writer);
+        }
+
+        private class Writer : BinaryBufferWriter
+        {
+            public int EventsCount;
+            public DateTime? WritingStarted;
+
+            public Writer(long initialCapacity)
+                : base((int)initialCapacity)
             {
-                buffer.Write(eventsCount);
+                Endianness = Endianness.Big;
             }
 
-            await settings.StreamBinaryWriter.WriteAsync(settings.StreamName, buffer.FilledSegment, eventsCount).ConfigureAwait(false);
+            public long RemainingCapacity => Buffer.Length - Position;
 
-            buffer.Reset();
-            buffer.Write(0);
-            eventsCount = 0;
+            public void StartEvent()
+            {
+                if (EventsCount == 0)
+                {
+                    Write(0);
+                    WritingStarted = DateTime.UtcNow;
+                }
+
+                EventsCount++;
+            }
+
+            public void EndWriting()
+            {
+                using (this.JumpTo(0))
+                    Write(EventsCount);
+            }
+
+            public void Reset()
+            {
+                base.Reset();
+                EventsCount = 0;
+            }
         }
     }
 }
+
+#endif
--- Vostok.Hercules.Consumers/StreamBinaryEventsWriterSettings.cs
+++ Vostok.Hercules.Consumers/StreamBinaryEventsWriterSettings.cs
@@ -1,4 +1,11 @@
-﻿using JetBrains.Annotations;
+﻿#if NET6_0_OR_GREATER
+
+using System;
+using JetBrains.Annotations;
+using Vostok.Commons.Time;
+using Vostok.Configuration.Primitives;
+using Vostok.Metrics;
+using Vostok.Tracing.Abstractions;
 
 namespace Vostok.Hercules.Consumers
 {
@@ -15,6 +22,15 @@ namespace Vostok.Hercules.Consumers
 
         public string StreamName { get; }
 
-        public int BufferCapacityLimit { get; set; } = 128 * 1024 * 1024;
+        public IMetricContext MetricContext { get; set; } = new DevNullMetricContext();
+        public ITracer Tracer { get; set; } = TracerProvider.Get();
+        
+        public DataSize WriterCapacity { get; set; } = 4.Megabytes();
+        public int WritersPoolCapacity { get; set; } = 8;        
+        public TimeSpan WriterMaxTtl { get; set; } = 5.Seconds();
+        
+        public int MaximumEventSize { get; set; } = 128 * 1024;
     }
 }
+
+#endif
